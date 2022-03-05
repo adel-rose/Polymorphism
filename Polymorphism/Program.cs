fa315ecd5710a21fab8577eae77c590870238eec
aFewPuppet <adelrose911@gmail.com>
2022-03-05 07:56:35 +0400

    first commit

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();  
+            Boat boat = new Boat();
+
+            Vehicle [] vehicles = { car, bicycle, boat };
+
+            foreach(Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
+
+        }
+    }
+}
---
+++ Polymorphism/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+     class Vehicle
+    {
+
+        public virtual void Go() 
+        {
+            Console.WriteLine("Vehicle is moving!");
+        }
+        
+    }
+}
